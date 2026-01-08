576dbb559e1eaa3710a095ac166bb7ced89db598
ekodaeng <instamakassar@gmail.com>
2026-01-08 18:46:17 +0800

    Aktifkan fitur PWA CTGOLD

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Buffer } from 'buffer';
+import { registerSW } from 'virtual:pwa-register';
 
 window.Buffer = Buffer;
 (window as any).global = window;
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
